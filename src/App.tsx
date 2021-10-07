9b4e5a50d8314b354a78b71f6d4eeec936a9a432
ong-ar <reclusis@gmail.com>
2021-10-07 12:20:21 +0900

    add node-sass & test css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
