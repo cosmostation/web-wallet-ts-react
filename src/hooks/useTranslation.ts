b6f7632e6ac4cc3bf3a188e45d53cdf3e014340f
ong-ar <reclusis@gmail.com>
2021-10-18 16:07:39 +0900

    del translation hook

--- src/hooks/useTranslation.ts
+++
@@ -1,8 +0,0 @@
-import type { UseTranslationOptions } from 'react-i18next';
-import { useTranslation as UseBaseTranslation } from 'react-i18next';
-
-export function useTranslation(ns?: 'common' | 'component' | 'page', options?: UseTranslationOptions) {
-  const translation = UseBaseTranslation(ns, options);
-
-  return translation;
-}
