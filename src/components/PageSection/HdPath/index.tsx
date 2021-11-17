bdbdeeb5278c6b3f0861a520316531a2a65c7fb7
ong-ar <reclusis@gmail.com>
2021-11-17 10:58:26 +0900

    add hdpath info

--- src/components/PageSection/HdPath/index.tsx
+++ src/components/PageSection/HdPath/index.tsx
@@ -8,6 +8,7 @@ import type { TextFieldProps } from '@mui/material';
 import { IconButton, TextField } from '@mui/material';
 
 import Button from '~/components/Button';
+import Dialog from '~/components/Dialog';
 import Signin from '~/components/Keystation/Signin';
 import { CHAIN } from '~/constants/chain';
 import { useCurrentChain } from '~/hooks/useCurrentChain';
@@ -30,6 +31,7 @@ export default function HdPath({ className }: HdPathProps) {
   const currentWallet = useCurrentWallet();
   const setIsShowLoader = useSetRecoilState(loaderState);
   const setWalletInfo = useSetRecoilState(walletInfoState);
+  const [isOpenedPathInfo, setIsOpenedPathInfo] = useState(false);
 
   const { t } = useTranslation();
 
@@ -112,6 +114,7 @@ export default function HdPath({ className }: HdPathProps) {
             sx={{
               color: '#5DCFDB',
             }}
+            onClick={() => setIsOpenedPathInfo(true)}
           >
             <HelpOutlineRoundedIcon />
           </IconButton>
@@ -187,6 +190,7 @@ export default function HdPath({ className }: HdPathProps) {
           }}
         />
       )}
+      <DialogPathInfo open={isOpenedPathInfo} onClose={() => setIsOpenedPathInfo(false)} />
     </>
   );
 }
@@ -214,5 +218,85 @@ function Input(props: TextFieldProps) {
       }}
       {...props}
     />
+  );
+}
+
+type DialogPathInfoProps = {
+  open: boolean;
+  onClose?: () => void;
+};
+
+function DialogPathInfo({ open, onClose }: DialogPathInfoProps) {
+  const { i18n } = useTranslation();
+
+  return (
+    <Dialog open={open} onClose={onClose} title="HD derivation path?">
+      <div
+        style={{
+          wordWrap: 'break-word',
+          fontSize: '1.4rem',
+          whiteSpace: 'pre-wrap',
+          padding: '0 2rem 2rem',
+          lineHeight: '1.6',
+          overflow: 'auto',
+        }}
+      >
+        {i18n.language === 'ko'
+          ? `코스모스테이션 웹 지갑은 HD월렛을 지원합니다. HD월렛을 사용하면 하나의 니모닉 시드키로 무한대의 지갑을 사용할 수 있게 됩니다.
+
+"지갑 경로"란, HD월렛 내에 사용하고자 하는 지갑의 정확한 위치를 알려주는 역할을 하는 포인터입니다. 니모닉 시드키는 무한대의 하위 지갑을 생성할 수 있는 마스터 키 입니다—"지갑 경로"는 마스터 키 에서 생성된 무한대의 지갑 목록에서 사용하고자 하는 특정 지갑으로 사용자를 안내하는 역할을 합니다.
+
+코스모스의 "지갑 경로"는 44/118/0/0/0 로 시작합니다. "0"을 원하는 번호로 변경하여 사용하고자 하는 지갑을 액세스 할 수 있습니다.
+
+예를 들어, 코스모스테이션 모바일 지갑에서는 하나의 니모닉 시드키로 최대 5개의 지갑을 사용할 수 있도록 서비스를 제공하고 있습니다. 코스모스테이션 모바일 지갑에선, 이 5개 지갑의 "지갑 경로" 설정을 아래와 같이 해놓았습니다.
+
+44/118/0/0/0
+44/118/0/0/1
+44/118/0/0/2
+44/118/0/0/3
+44/118/0/0/4
+
+아래는 사용자가 직접 설정할 수 있는 여러가지 "지갑 경로"의 예 입니다:
+
+44/118/0/0/79
+44/118/0/3/56
+44/118/0/18/2
+44/118/2/0/3
+44/118/14/5/4
+
+위의 예시처럼, 코스모스테이션 웹 지갑에서는 하나의 니모닉 시드키로 무한대의 코스모스 지갑을 생성할 수 있습니다.
+
+위의 예시와 같이 커스텀 "지갑 경로"를 사용하는 경우에는, 반드시 본인이 설정한 "지갑 경로"를 잊지 않고 기억해야 합니다—만약 "지갑 경로"를 잊었을 경우에는, 지갑의 정확한 경로를 되찾는데 적게는 몇일이, 많게는 몇달이 걸릴 수 있습니다.
+
+"지갑 경로"를 기본 설정으로 사용하기를 원하신다면, 기본으로 설정되어있는 경로 (44/118/0/0/0)를 변경하지 않고 그대로 사용하시기 바랍니다.`
+          : `Cosmostation web wallet supports HD wallets. With an HD wallet, you can access a virtually infinite number of wallets with a single mnemonic phrase.
+
+A "Derivation path" is a pointer that directs you to a specific wallet you wish to access. Think of your mnemonic phrase as a master key that unlocks an infinite number of sub wallets—a "derivation path" directs you to a specific wallet you wish to access from this list of infinite wallets generated from your master mnemonic phrase.
+
+"Derivation paths" for Cosmos begin with 44/118/0/0/0. Replace the "0"s with any number you wish in order to access the wallet you want to use.
+
+For example, in Cosmostation mobile wallet, you can access 5 different wallets with a single mnemonic phrase. In the application, the default "derivation path" settings for these 5 wallets are:
+
+44/118/0/0/0
+44/118/0/0/1
+44/118/0/0/2
+44/118/0/0/3
+44/118/0/0/4
+
+Below are examples of "derivation paths" you could use:
+
+44/118/0/0/79
+44/118/0/3/56
+44/118/0/18/2
+44/118/2/0/3
+44/118/14/5/4
+
+Like the example above, you can access an infinite number of Cosmos wallets with a single mnemonic phrase.
+
+If you use a custom "derivation path" like the examples above, please take caution and always remember your specific "derivation path"—if you forget your path, it could take days, years, maybe decades to find it again.
+
+If you wish to use the wallet without using a custom "derivation path", leave the default "derivation path" (44/118/0/0/0) unchanged and proceed to access your wallet.`}
+      </div>
+    </Dialog>
   );
 }
