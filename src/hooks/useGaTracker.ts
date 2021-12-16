c23d5535d85993acf9ae7cf0faf52427286817c5
ong-ar <reclusis@gmail.com>
2021-12-16 12:40:17 +0900

    add ga

---
+++ src/hooks/useGaTracker.ts
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
+import { useLocation } from 'react-router-dom';
+
+export function useGaTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+}
