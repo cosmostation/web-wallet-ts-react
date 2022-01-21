4a810727ec0b530496eaa81a5c18d7481face88c
ong-ar <reclusis@gmail.com>
2022-01-21 12:31:00 +0900

    add osmosis chain

--- src/constants/chain.ts
+++ src/constants/chain.ts
@@ -28,6 +28,7 @@ export const CHAIN = {
   LUM: 'lum',
   STARGAZE: 'stargaze',
   CHIHUAHUA: 'chihuahua',
+  OSMOSIS: 'osmosis',
 } as const;
 
 // chain info key === path
@@ -1037,6 +1038,45 @@ export const chains = {
       withdrawReward: '0.0035',
       withdrawCommission: '0.0035',
       modifyWithdrawAddress: '0.0035',
+    },
+    gas: {
+      default: '200000',
+      delegate: '200000',
+      undelegate: '200000',
+      redelegate: '300000',
+      withdraw: '200000',
+      withdrawReward: '200000',
+      withdrawCommission: '200000',
+      modifyWithdrawAddress: '200000',
+    },
+  },
+  [CHAIN.OSMOSIS]: {
+    chainId: 'osmosis-1',
+    name: 'osmosis',
+    path: CHAIN.OSMOSIS,
+    imgURL: `${baseURL}/osmosis.png`,
+    wallet: {
+      hdPath: '44/118/0/0/0',
+      prefix: 'osmo',
+      support: { ledger: true, keystation: true },
+      isProto: true,
+    },
+    lcdURL: 'https://lcd-osmosis.cosmostation.io',
+    symbolName: 'OSMO',
+    denom: 'uosmo',
+    decimal: 6,
+    coingeckoId: 'osmosis',
+    validatorIconDirectory: 'osmosis',
+    mintscanPath: 'osmosis',
+    fee: {
+      default: '0.005',
+      delegate: '0.005',
+      undelegate: '0.005',
+      redelegate: '0.005',
+      withdraw: '0.005',
+      withdrawReward: '0.005',
+      withdrawCommission: '0.005',
+      modifyWithdrawAddress: '0.005',
     },
     gas: {
       default: '200000',
