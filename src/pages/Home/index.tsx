4cd5fb276a25b14ade5b1708b7a51af5d91dd8a1
ong-ar <reclusis@gmail.com>
2022-08-01 15:57:39 +0900

    test autosign

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { cosmos } from '@cosmostation/extension-client';
 
 import DialogWalletConnect from '~/components/Dialog/DialogWalletConnect';
 import Layout from '~/components/Layout';
@@ -20,9 +22,45 @@ export default function Home() {
     history.push(`/${currentChain.path}/wallet`);
   };
 
+  const handleOnSetAutoSign = async () => {
+    try {
+      const provider = await cosmos();
+      await provider.autoSign.set('cosmos', 30);
+    } catch {
+      console.log('error');
+    }
+  };
+
+  const handleOnGetAutoSign = async () => {
+    try {
+      const provider = await cosmos();
+      console.log('getAutoSign', await provider.autoSign.get('cosmos'));
+    } catch {
+      console.log('error');
+    }
+  };
+
+  const handleOnDeleteAutoSign = async () => {
+    try {
+      const provider = await cosmos();
+      console.log('deleteAutoSign', await provider.autoSign.delete('cosmos'));
+    } catch {
+      console.log('error');
+    }
+  };
+
   return (
     <Layout>
       <>
+        <button type="button" onClick={handleOnSetAutoSign}>
+          set
+        </button>
+        <button type="button" onClick={handleOnGetAutoSign}>
+          get
+        </button>
+        <button type="button" onClick={handleOnDeleteAutoSign}>
+          delete
+        </button>
         <div className={styles.container}>
           <div className={styles.mainImg} />
           <button type="button" onClick={handleOnOpenConnect} className={styles.connectButton}>
